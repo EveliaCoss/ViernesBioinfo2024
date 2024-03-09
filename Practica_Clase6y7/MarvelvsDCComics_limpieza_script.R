51632ae6b2ff60a1772a52b611be0c440c2fd8e6
Evelia Lorena Coss-Navarrete <49405921+EveliaCoss@users.noreply.github.com>
2024-03-08 18:08:00 -0600

    Update MarvelvsDCComics_limpieza_script.R

--- Practica_Clase6y7/MarvelvsDCComics_limpieza_script.R
+++ Practica_Clase6y7/MarvelvsDCComics_limpieza_script.R
@@ -1,10 +1,21 @@
 ######
-# Script de clase "Intro a R"
-# Evelia Coss
-# 8 de marzo 2024
+# Script: Manipulacion y limpieza de los dataframes de superheroes
+# Autor: Evelia Coss
+# Fecha: 8 de marzo 2024
+# En este script veremos la importacion  y limpieza de datos a partir de 3 archivos CSV.
+# Argumentos:
+# Paso 1 . Crear un Project en R y crear una carpeta "data/".
+# Paso 2. Descargar los archivos charactersStats.csv, heroesInformation.csv y superHeroPowers.csv
+# del Github https://github.com/cosmoduende/r-marvel-vs-dc/tree/main/dataset_shdb, 
+# los archivos deben estar almacenados en la carpeta "data/".
+# - Input: charactersStats.csv, heroesInformation.csv y superHeroPowers.csv
+# - Output: 
+#   - marvelDcInfo: dataframe sin duplicados, conformado de 4 columnas ("Name", "Gender", "Race", "Publisher")
+#   - infoPowers y  infoStats:  dataframes solo con la modificacion en la columna "Name"
+#   Las 3 variables se almacenaron en "Info_data.RData"
 #######
 
-# --- Importar datos ----
+# --- Paso 3. Importar los datos en R ----
 
 indir = "C:/Users/ecoss/OneDrive - CINVESTAV/Documentos/Posdoc_LIIGH/VieRnesBioinfo/ViernesBioinfo_2024/Presentaciones/data/"
 outdir = "C:/Users/ecoss/OneDrive - CINVESTAV/Documentos/Posdoc_LIIGH/VieRnesBioinfo/ViernesBioinfo_2024/Presentaciones/"
@@ -16,11 +27,11 @@ infoStats <- read.csv(paste0(indir,"charactersStats.csv"), na.strings = "")
 
 # --- Manipulacion de datos ----
 
-# Renombrar la columna **Name** en todos los dataframe
+# Paso 4. Renombrar la columna **Name** en todos los dataframe
 colnames(infoCharacters)[colnames(infoCharacters) == "name"] <- "Name"
 colnames(infoPowers)[colnames(infoPowers) == "hero_names"] <- "Name"
 
-# Seleccionar SOLO los datos de Marvel Comics y DC Comics
+# Paso 5. Seleccionar SOLO los datos de Marvel Comics y DC Comics
 unique(infoCharacters$Publisher)
 
 marvelDcInfo <- infoCharacters[(infoCharacters$Publisher == "Marvel Comics" | infoCharacters$Publisher == "DC Comics"), ]
@@ -42,14 +53,14 @@ marvelDcInfo[marvelDcInfo$Name == "Batman", ]
 subset(marvelDcInfo, Name == "Batman")
 
 ## ----Limpieza de datos -----
-## Eliminar duplicados
+## Paso 6. Eliminar duplicados
 marvelDcInfo <- marvelDcInfo[!duplicated(marvelDcInfo$Name), ]
 
-# Seleccionar columnas
+# Paso 7. Seleccionar columnas
 marvelDcInfo <- marvelDcInfo[, c("Name", "Gender", "Race", "Publisher")]
 head(marvelDcInfo, 3)
 
-# Cambiar formatos en algunas columnas
+# Paso 8. Cambiar formatos en algunas columnas
 str(marvelDcInfo)
 
 marvelDcInfo$Name <- as.factor(marvelDcInfo$Name)
